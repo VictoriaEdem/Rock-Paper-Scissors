3dd2fa551e9d6ef7ea0bdffdceea9cfd3fd9d6cd
Victoria Edem <88201413+VictoriaEdem@users.noreply.github.com>
2022-06-13 13:17:20 +0000

    Update main.py

--- main.py
+++ main.py
@@ -1,50 +1,49 @@
 import random
 
+possible_options = ["R", "P", "S"]
 opt = {
     'R': "Rock",
     'S': "Scissors",
     'P': "Paper"}
 
 while True:
-    possible_options = ["R", "P", "S"]
+    user_input = input("Pick an option between 'R', 'P', or 'S': ").upper()
 
-    CPU = random.choice(possible_options)
-    user_option = None
-    user_option = input("Pick an option between 'R', 'P', or 'S': ").upper()
-
-
-    if user_option == CPU:
-        print(f"Player ({opt.get(user_option)}) : CPU ({opt.get(CPU)})")  
+    if user_input not in possible_options:
+        print("Error. Pick another option.")
+    
+    CPU = random.choice(possible_options)    
+        
+    if user_input == CPU:
+        print(f"Player ({opt.get(user_input)}) : CPU ({opt.get(CPU)})")  
         print("The game is tied. Play again.")
-    elif user_option == "S":
+    elif user_input == "S":
         if CPU == "R":
-            print(f"Player ({opt.get(user_option)}) : CPU ({opt.get(CPU)})")
-            print("You lost. Rock beats Scissors") 
+            print(f"Player ({opt.get(user_input)}) : CPU ({opt.get(CPU)})")
+            print("CPU wins, you lost. Rock beats Scissors.") 
             break
         if CPU == "P":
-            print(f"Player ({opt.get(user_option)}) : CPU ({opt.get(CPU)})")
-            print("You win! Rock beats Paper")
+            print(f"Player ({opt.get(user_input)}) : CPU ({opt.get(CPU)})")
+            print("You win! Scissors beats Paper.")
             break
-    elif user_option == "R":
+    elif user_input == "R":
         if CPU == "P":
-            print(f"Player ({opt.get(user_option)}) : CPU ({opt.get(CPU)})")
-            print("You lost. Paper beats Rock")
+            print(f"Player ({opt.get(user_input)}) : CPU ({opt.get(CPU)})")
+            print("CPU wins, you lost. Paper beats Rock.")
             break
         if CPU == "S":
-            print(f"Player ({opt.get(user_option)}) : CPU ({opt.get(CPU)})")
-            print("You win! Rock beats Scissors")
+            print(f"Player ({opt.get(user_input)}) : CPU ({opt.get(CPU)})")
+            print("You win! Rock beats Scissors.")
             break
-    elif user_option == "P":
+    elif user_input == "P":
         if CPU == "S":
-            print(f"Player ({opt.get(user_option)}) : CPU ({opt.get(CPU)})")
-            print("You lost. Scissors beats Paper")
+            print(f"Player ({opt.get(user_input)}) : CPU ({opt.get(CPU)})")
+            print("CPU wins, you lost. Scissors beats Paper.")
             break
         if CPU == "R":
-            print(f"Player ({opt.get(user_option)}) : CPU ({opt.get(CPU)})")
-            print("You win! Paper beats Rock")
+            print(f"Player ({opt.get(user_input)}) : CPU ({opt.get(CPU)})")
+            print("You win! Paper beats Rock.")
             break
-    if user_option not in possible_options:
-        print("Error. Try Again")
     else:
         continue           
 
